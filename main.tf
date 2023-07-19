e8ae89faa281c79d0d8c926f8deea75d89205a70
tannv-devv <tannvde31@gmail.com>
2023-07-19 10:37:33 +0700

    update jenkins file

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "ami" {
 
 resource "aws_instance" "server" {
   ami           = data.aws_ami.ami.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   lifecycle {
     create_before_destroy = true
